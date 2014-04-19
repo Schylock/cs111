c58505fefaaed35657da3f710a9d8d16cf58ba30
arjunpatel422 <arjunpatel422@gmail.com>
2014-04-18 19:11:39 -0700

    Execution Partial Implement
    This is everything we did yesterday.

--- lab1-Skeleton/execute-command.c
+++ lab1-Skeleton/execute-command.c
@@ -4,7 +4,11 @@
 #include "command-internals.h"
 #include <stdlib.h>
 #include <error.h>
-
+#include <string.h>
+#include <unistd.h>
+#include<stdio.h>
+#include <sys/types.h>
+#include <fcntl.h>
 /* FIXME: You may need to add #include directives, macro definitions,
    static function definitions, etc.  */
 
@@ -13,16 +17,99 @@ int command_status (command_t c)
   return c->status;
 }
 
-void execute_command (command_t c, int time_travel)
+void runSimpleCommand(command_t c)
 {
-	c=NULL;
-	time_travel=0;
-	if(time_travel)
-		time_travel=time_travel;
-	if(c)
-		c=c;
-  /* FIXME: Replace this with your implementation.  You may need to
-     add auxiliary functions and otherwise modify the source code.
-     You can also use external functions defined in the GNU C Library.  */
-  error (1, 0, "command execution not yet implemented");
+	pid_t pid=fork();
+	if(pid==0)
+	{
+		int inputFD;//if input, change to input else change to stdin
+		if(c->input)
+		{
+			inputFD=open(c->input,O_RDWR);
+			close(0);
+			dup(inputFD);
+			close(inputFD);
+		}
+		int outputFD;//if output
+		if(c->output)
+		{
+			outputFD=open(c->output,O_RDWR);
+			printf("After opening %d\n",outputFD);
+			close(1);
+			dup(outputFD);
+			close(outputFD);
+		}
+		c->status=execvp(c->u.word[0],c->u.word);
+	}
+	else
+	while(pid!=wait(&(c->status)))
+	{
+		continue;
+	} 
+}
+
+void runPipeCommand(command_t c)
+{
+	int fd[2];
+	pid_t pid[2];
+	pipe(fd);
+	pid[0]=fork();
+	if(pid[0]==0)
+	{
+		pid[1]=fork();
+		if(pid[1]==0)
+		{
+			close(fd[1]);
+			dup2(fd[0],0);
+			close(fd[0]);
+			c->status=execute_command(c->u.command[1],0);
+			exit(c->status);
+		}
+		else
+		{
+			close(fd[0]);
+			dup2(fd[1],1);
+			close(fd[1]);
+			execute_command(c->u.command[0],0);
+			exit(0);
+		}
+	}
+	else
+	waitpid(pid[0],&(c->status),0);
+}
+
+int execute_command (command_t c, int time_travel)
+{
+	pid_t pid;
+	switch (c->type)
+    {
+		case AND_COMMAND:
+		c->status=execute_command(c->u.command[0],0);
+		if(c->status==0)
+			c->status=execute_command(c->u.command[1],0);
+		break;
+		case OR_COMMAND:
+		c->status=execute_command(c->u.command[0],0);
+		if(c->status)
+			c->status=execute_command(c->u.command[1],0);
+		break;
+		case SEQUENCE_COMMAND:
+		c->status=execute_command(c->u.command[0],0);
+		c->status=execute_command(c->u.command[1],0);
+		break;
+		case PIPE_COMMAND:
+		runPipeCommand(c);
+		break;
+		case SIMPLE_COMMAND:
+		runSimpleCommand(c);
+		break;
+		case SUBSHELL_COMMAND:
+		c->status=execute_command(c->u.subshell_command,0);
+		break;
+		default:
+		abort ();
+	}
+
+	
+	return c->status;
 }
